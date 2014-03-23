672fe2c8492dfbfa304e9cb138b0dba1117f0707
festerman <staffan@andersson-bergh.se>
2014-03-23 20:55:03 +0100

    Added tests/specs ... not all passing, yet.

---
+++ BFiles/src/test/scala/se/bfiles/entities/DonorSpec.scala
@@ -0,0 +1,38 @@
+package se.bfiles.entities
+
+import org.specs2.mutable.Specification
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import scala.collection.immutable.HashSet
+import se.bfiles.operations.Sampling
+
+@RunWith(classOf[JUnitRunner])
+class DonorSpec extends Specification {
+
+	val d = new Donor ()
+	val idstring = "id"
+	var samples1 = new HashSet[PrimarySample]()
+	samples1 += new PrimarySample(d)
+
+  "A Donor " should {
+    "have id" in {
+      d.id(idstring)
+      d.id() must_== idstring
+    }
+    
+   "know if (s)he was sampled" in {
+	 val s = Sampling(d,samples1)
+	 d.wasSampled must beTrue
+   }
+
+   "know if (s)he was not sampled" in {
+	 d.wasSampled must beFalse
+   }
+
+   "recognize if (s)he was part of a specific sampling" in {
+     val s = Sampling(d,samples1)
+     d.wasSampled(s) must beTrue
+   }
+  }
+
+}
